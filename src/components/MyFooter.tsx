7f7e3f7a2b552eafd115d8ad019cbf50417f5373
LauraI-S <laura.ikvaiszabo@gmail.com>
2024-02-29 14:46:51 +0100

    footer changed

--- src/components/MyFooter.tsx
+++ src/components/MyFooter.tsx
@@ -2,21 +2,23 @@ import React from "react";
 
 function MyFooter() {
   return (
-    <div className="bg-cyan-700 p-4 text-white">
+    <div className="bg-gradient-to-r from-purple-500 to-indigo-600 p-6 text-white">
       <div className="container mx-auto">
-        <div className="flex items-center justify-between">
-          <a href="/about" className="text-white hover:underline">
-            LiLu
-          </a>
-          <a
-            href="https://www.codeacademyberlin.com/"
-            className="text-white hover:underline"
-          >
-            @ CodeAcademy Berlin
-          </a>
-
-          <div className="flex space-x-4">
-            <a href="/contact" className="text-white hover:underline">
+        <div className="flex flex-col md:flex-row items-center justify-between">
+          <div className="flex items-center">
+            <a href="/about" className="text-xl font-bold hover:underline mr-4">
+              LiLu
+            </a>
+            <p className="text-sm md:text-base">Crafting Dreams into Reality</p>
+          </div>
+          <div className="flex items-center mt-4 md:mt-0">
+            <a
+              href="https://www.codeacademyberlin.com/"
+              className="text-xl font-bold hover:underline mr-8"
+            >
+              @ CodeAcademy Berlin
+            </a>
+            <a href="/contact" className="text-xl font-bold hover:underline">
               Contact
             </a>
           </div>
